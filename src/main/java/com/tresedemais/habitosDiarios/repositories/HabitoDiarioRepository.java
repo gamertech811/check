a5225dfdcc38896a1908ea3064c97dfaf77c93ae
jascinto leite aquino rego <165964313+gamertech811@users.noreply.github.com>
2025-10-06 17:22:07 -0300

    feat: services

--- src/main/java/com/tresedemais/habitosDiarios/repositories/HabitoDiarioRepository.java
+++ src/main/java/com/tresedemais/habitosDiarios/repositories/HabitoDiarioRepository.java
@@ -4,5 +4,5 @@ import com.tresedemais.habitosDiarios.models.HabitoDiario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HabitoDiarioRepository extends JpaRepository<HabitoDiario, Integer> {
-
+    public findByDate(Date data);
 }
--- src/main/java/com/tresedemais/habitosDiarios/services/ServiceHabDiario.java
+++ src/main/java/com/tresedemais/habitosDiarios/services/ServiceHabDiario.java
@@ -4,12 +4,31 @@ package com.tresedemais.habitosDiarios.services;
 import com.tresedemais.habitosDiarios.models.HabitoDiario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServiceHabDiario {
     @Autowired
-    
+    HabitoDiarioRepository habdiaRepo;
     public HabitoDiario criar(HabitoDiario habdia){
+        return habdiaRepo.save(habdia);
+    }
 
+    public List<HabitoDiario> pegar(){
+        return habdiaRepo.findAll();
+    }
+
+    public void excluir(Integer id){
+        habdiaRepo.deleteById(id);
+    }
+
+    public HabitoDiario acharPorData(Date data){
+        Optional<HabitoDiario> pordata = habdiaRepo.findByDate(data);
+        if(pordata.ispresent()){
+            return pordata.get();
+        }else{
+            return null;
+        }
     }
 }
